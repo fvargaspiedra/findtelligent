2e6e294b1958bf7590babbfe2669212278b5f55f
Francisco Vargas <fvargasp@akamai.com>
2017-11-15 02:00:01 -0600

    Chrome extension is now retreiving the query and url and calling local API and parse the json

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -34,8 +34,6 @@ function getCurrentTabUrl(callback) {
     // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    // console.log(url);
-
     callback(url);
   });
 
@@ -49,60 +47,17 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
 
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
 
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-function callApi(){
-  var userInput = document.getElementById("userInput").value;
-  console.log(userInput);
+function callApi(url){
+  var query = document.getElementById("userInput").value;
+  console.log(query);
+  console.log(url);
 
   var xmlhttp = new XMLHttpRequest();
-  var url = "http://localhost:5000/api/v1/getbyurl?q=awards%20received&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTim_Berners-Lee&method=dd";
+  var apiUrl = "http://localhost:5000/api/v1/getbyurl?q=" + encodeURI(query) + "&url=" + encodeURI(url) + "&method=dd";
+
+  console.log(apiUrl)
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -110,7 +65,7 @@ function callApi(){
       myFunction(myArr);
     }
   };
-  xmlhttp.open("GET", url, true);
+  xmlhttp.open("GET", apiUrl, true);
   xmlhttp.send();
 
   function myFunction(arr) {
@@ -123,7 +78,7 @@ function callApi(){
 }
 
 window.onload=function(){
-  document.getElementById("submitQuery").addEventListener("click", callApi);
+  document.getElementById("submitQuery").addEventListener("click", () => { getCurrentTabUrl((url) => { callApi(url) }); });
 
   document.getElementById("userInput").addEventListener("keyup", function(event) {
     event.preventDefault();
@@ -132,33 +87,3 @@ window.onload=function(){
     }
   });
 }
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-// document.addEventListener('DOMContentLoaded', () => {
-//   getCurrentTabUrl((url) => {
-//     var dropdown = document.getElementById('dropdown');
-
-//     // Load the saved background color for this page and modify the dropdown
-//     // value, if needed.
-//     getSavedBackgroundColor(url, (savedColor) => {
-//       if (savedColor) {
-//         changeBackgroundColor(savedColor);
-//         dropdown.value = savedColor;
-//       }
-//     });
-
-//     // Ensure the background color is changed and saved when the dropdown
-//     // selection changes.
-//     dropdown.addEventListener('change', () => {
-//       changeBackgroundColor(dropdown.value);
-//       saveBackgroundColor(url, dropdown.value);
-//     });
-//   });
-// });
