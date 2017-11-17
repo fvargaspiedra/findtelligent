1dbc8246c293333c20893bedcf0975b909be390f
Francisco Vargas <fvargasp@akamai.com>
2017-11-16 23:37:30 -0600

    Findtelligent finding! No unmark and problem with wikipedia's cite numbers

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -2,8 +2,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   if(request.todo == "highlight"){
     console.log(request.regex);
     console.log("Hola");
+    var options = {
+      "acrossElements": true,
+    };
     var context = document.body; // requires an element with class "context" to exist
     var instance = new Mark(context);
-    instance.markRegExp(/computer/gmi); // will mark the keyword "test"
+    //var pattern = new RegExp('Berners-Lee[^a-zA-Z0-9]*Mary[^a-zA-Z0-9]*Lee[^a-zA-Z0-9]*Woods[^a-zA-Z0-9]*Awards')
+    for(i = 0; i < request.regex.length; i++) {
+      var regex = new RegExp(String(request.regex[i].regexp), 'gmi');
+      instance.markRegExp(regex, options); // will mark the keyword "test"
+    }
   }
 })
