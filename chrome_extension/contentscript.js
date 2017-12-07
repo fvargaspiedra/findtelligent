ec14d75ef5d49e6bf749058c99268cd033188ed0
Francisco Vargas <fvargasp@akamai.com>
2017-12-06 22:49:11 -0600

    Clean Chrome Extension code

--- chrome_extension/contentscript.js
+++ chrome_extension/contentscript.js
@@ -1,17 +1,14 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   if(request.todo == "highlight"){
-    console.log(request.regex);
-    console.log("Hola");
     var options = {
       "acrossElements": true,
     };
-    var context = document.body; // requires an element with class "context" to exist
+    var context = document.body;
     var instance = new Mark(context);
-    //var pattern = new RegExp('Berners-Lee[^a-zA-Z0-9]*Mary[^a-zA-Z0-9]*Lee[^a-zA-Z0-9]*Woods[^a-zA-Z0-9]*Awards')
     instance.unmark();
     for(i = 0; i < request.regex.length; i++) {
       var regex = new RegExp(String(request.regex[i].regexp), 'gmi');
-      instance.markRegExp(regex, options); // will mark the keyword "test"
+      instance.markRegExp(regex, options);
     }
   }
 })
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,65 +1,21 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
-
     callback(url);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
-
-
 
 function callApi(url){
   var query = document.getElementById("userInput").value;
-  console.log(query);
-  console.log(url);
-
   var xmlhttp = new XMLHttpRequest();
   var apiUrl = "http://localhost:5000/api/v1/getbyurl?q=" + encodeURI(query) + "&url=" + encodeURI(url) + "&method=dd";
-
-  console.log(apiUrl)
-
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var myArr = JSON.parse(this.responseText);
@@ -68,28 +24,6 @@ function callApi(url){
   };
   xmlhttp.open("GET", apiUrl, true);
   xmlhttp.send();
-
-  // var context = document.body; // requires an element with class "context" to exist
-  // console.log(context)
-  // var instance = new Mark(context);
-  // instance.mark("graduation"); // will mark the keyword "test"
-
-  // var markInstance = new Mark(document.querySelector("body"));
-
-  // markInstance.mark("senior");
-
-  // var options = {
-  //     "acrossElements": true,
-  // };
-
-  // Remove previous marked elements and mark
-  // the new keyword inside the context
-  // markInstance.unmark({
-  //   done: function(){
-
-  //   }
-  // });
-
   function myFunction(arr) {
     var out = "";
     var i;
@@ -97,20 +31,11 @@ function callApi(url){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
       chrome.tabs.sendMessage(tabs[0].id, {todo: "highlight", regex: arr})
     })
-    console.log(arr.type)
-    // for(i = 0; i < arr.length; i++) {
-    //   re = String(arr[i].regexp)
-    //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    //     chrome.tabs.sendMessage(tabs[0].id, {todo: "highlight", regex: re})
-    //   })
-    //   console.log(arr[i].regexp);
-    // }
   }
 }
 
 window.onload=function(){
   document.getElementById("submitQuery").addEventListener("click", () => { getCurrentTabUrl((url) => { callApi(url) }); });
-
   document.getElementById("userInput").addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
